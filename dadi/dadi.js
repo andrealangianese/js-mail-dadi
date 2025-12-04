2a33db151d9f7fb350e3d2068126cdc697e9a459
andrealangianese <andre.langia.01@gmail.com>
2025-12-04 17:40:18 +0100

    feat: ex dadi finito

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -12,3 +12,20 @@
 // si ma noi cosa vogliamo fare?
 // torniamo a scrivere in italiano
 // proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+// creo numeri da 1 a 6 (per i due giocatori) e faccio generare 2 numeri
+
+// Genera un numero intero casuale tra 1 e 6 (inclusi)
+// const lancioDado1 = Math.floor(Math.random() * 6) + 1;
+// const lancioDado2 = Math.floor(Math.random() * 6) + 1;
+
+if (lancioDado1 >lancioDado2) {
+    console.log("hai vinto tu")
+} else if (lancioDado2 > lancioDado1) {
+    console.log("ritenta sarai più fortuanato");  
+} else {
+    console.log("avete fatto lo stesso numero,ritirate!");
+    
+}
+console.log(lancioDado1, "mio tiro");
+console.log(lancioDado2, "tiro computer");
